9a5b93a902cdc2979aa0a3a6d45a00a809af7efa
Guillaume Méroué <guillaume.meroue@ens-lyon.fr>
2022-10-02 16:02:33 +0200

    existance d'un transiant nft

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,10 @@ blueprint! {
     struct Styx {
         // Define what resources and data will be managed by Hello components
         sample_vault: Vault,
-        internal_authority : Vault
+        internal_authority : Vault,
+        stake : Vault,
+        //styx_adress : RessourceAddress,
+        transiant_ressource_adress: ResourceAddress
     }
 
     impl Styx {
@@ -20,10 +23,9 @@ blueprint! {
                 .divisibility(DIVISIBILITY_NONE)
                 .metadata("name", "RegulatedToken internal authority badge")
                 .burnable(rule!(allow_all), LOCKED)
-                .initial_supply(1);
+                .initial_supply(dec!("1"));
 
             let access_rule: AccessRule = rule!(require(internal_admin.resource_address()));
-
 
             let my_bucket: Bucket = ResourceBuilder::new_fungible()
                 .divisibility(DIVISIBILITY_MAXIMUM)
@@ -37,12 +39,29 @@ blueprint! {
                     access_rule.clone(),
                     MUTABLE(access_rule.clone())
                 )
-                .initial_supply(100);    
-            
+                .initial_supply(dec!("100"));
+
+            let styx_adress : ResourceAddress = my_bucket.resource_address();
+
+
+
+            let address = ResourceBuilder::new_non_fungible()
+                .metadata(
+                    "name",
+                    "Promise token for BasicFlashLoan - must be returned to be burned!",
+                )
+                .mintable(rule!(require(internal_admin.resource_address())), LOCKED) // 1
+                .burnable(rule!(require(internal_admin.resource_address())), LOCKED) // 1
+                .restrict_deposit(rule!(deny_all), LOCKED) // 1
+                .no_initial_supply();
+                
             // Instantiate a Hello component, populating its vault with our supply of 1000 HelloToken
             Self {
                 sample_vault: Vault::with_bucket(my_bucket),
-                internal_authority: Vault::with_bucket(internal_admin)
+                internal_authority: Vault::with_bucket(internal_admin),
+                transiant_ressource_adress : address,
+                stake : Vault::new(styx_adress),
+                //styx_adress,
             }
             .instantiate()
             .globalize()
@@ -56,5 +75,12 @@ blueprint! {
             // In this case, a bucket containing 1 HelloToken is returned
             self.sample_vault.take(1)
         }
+
+        pub fn stake_styx(&mut self, deposit : Bucket) -> Bucket {
+            info!("You are going to stake : {}", deposit.amount());
+            deposit
+        }
+
+
     }
 }
