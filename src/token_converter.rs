a7daf26754027b0fab5c9e900bae042efed5fe45
Guillaume Méroué <guillaume.meroue@ens-lyon.fr>
2022-10-06 23:00:48 +0200

    clean

--- src/token_converter.rs
+++ src/token_converter.rs
@@ -11,7 +11,7 @@ pub struct Receipt {
 blueprint! {
     struct Styx {
         // Define what resources and data will be managed by Hello components
-        sample_vault: Vault,
+        emission_vault: Vault,
         internal_authority : Vault,
         stake : Vault,
         styx_adress : ResourceAddress,
@@ -22,7 +22,7 @@ blueprint! {
         // Implement the functions and methods which will manage those resources and data
         
         // This is a function, and can be called directly on the blueprint once deployed
-        pub fn instantiate_stx() -> ComponentAddress {
+        pub fn instantiate() -> ComponentAddress {
 
             // Next we will create a badge we'll hang on to for minting & transfer authority
             let internal_admin: Bucket = ResourceBuilder::new_fungible()
@@ -61,7 +61,7 @@ blueprint! {
                 
             // Instantiate a Hello component, populating its vault with our supply of 1000 HelloToken
             Self {
-                sample_vault: Vault::with_bucket(my_bucket),
+                emission_vault: Vault::with_bucket(my_bucket),
                 internal_authority: Vault::with_bucket(internal_admin),
                 receipt_address : address,
                 stake : Vault::new(styx_adress),
@@ -74,13 +74,13 @@ blueprint! {
 
         // This is a method, because it needs a reference to self.  Methods can only be called on components
         pub fn free_token(&mut self) -> Bucket {
-            info!("My balance is: {} HelloToken. Now giving away a token!", self.sample_vault.amount());
+            info!("My balance is: {} HelloToken. Now giving away a token!", self.emission_vault.amount());
             // If the semi-colon is omitted on the last line, the last value seen is automatically returned
             // In this case, a bucket containing 1 HelloToken is returned
-            self.sample_vault.take(1)
+            self.emission_vault.take(1)
         }
 
-        pub fn stake_styx(&mut self, deposit : Bucket) -> (Bucket,Proof) {
+        pub fn stake(&mut self, deposit : Bucket) -> (Bucket,Proof) {
             assert!(deposit.resource_address() == self.stake.resource_address());
 
             info!("You are going to stake : {}", deposit.amount());
@@ -101,7 +101,7 @@ blueprint! {
             (receipt,deposit_proof)
         }
 
-        pub fn unstake_styx(&mut self, proof : Proof, amount: Decimal) -> Bucket {
+        pub fn unstake(&mut self, proof : Proof, amount: Decimal) -> Bucket {
             
             let resource_manager : &mut ResourceManager = borrow_resource_manager!(self.receipt_address);
 
@@ -126,6 +126,10 @@ blueprint! {
 
             self.stake.take(amount)
         }
+
+
+
+        
 
         /// Checks that a given [`Proof`] corresponds to a position and returns the associated
         /// [`ValidatedProof`]
