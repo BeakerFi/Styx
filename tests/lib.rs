ff3998483ac564aa39e3b2a76237e498c8b8bfb2
Guillaume Méroué <guillaume.meroue@ens-lyon.fr>
2022-10-08 19:53:09 +0200

    stake_test

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,4 +1,5 @@
 use radix_engine::ledger::*;
+use radix_engine::transaction::TransactionReceipt;
 use scrypto::core::NetworkDefinition;
 use scrypto::prelude::*;
 use scrypto_unit::*;
@@ -28,6 +29,15 @@ fn test_hello() {
         .entity_changes
         .new_component_addresses[0];
 
+    let stx = receipt
+        .expect_commit()
+        .entity_changes
+        .new_resource_addresses[1];
+
+    println!("The stx adress is {}",stx);
+
+    
+
     // Test the `free_token` method.
     let manifest = ManifestBuilder::new(&NetworkDefinition::simulator())
         .call_method(component, "free_token", args!())
@@ -40,7 +50,8 @@ fn test_hello() {
     let receipt = test_runner.execute_manifest_ignoring_fee(manifest, vec![public_key.into()]);
     println!("{:?}\n", receipt);
     receipt.expect_commit_success();
-        // Test the `free_token` method.
+    
+    // Test the `free_token` method.
         let manifest = ManifestBuilder::new(&NetworkDefinition::simulator())
         .call_method(component, "free_token", args!())
         .call_method(
@@ -52,6 +63,53 @@ fn test_hello() {
     let receipt = test_runner.execute_manifest_ignoring_fee(manifest, vec![public_key.into()]);
     println!("{:?}\n", receipt);
     receipt.expect_commit_success();
+
+
+
+
+        // Test the `stake` method.
+        let manifest = ManifestBuilder::new(&NetworkDefinition::simulator())
+        .call_method(account_component, "withdraw_by_amount", args!(dec!("1"),stx))
+        .take_from_worktop_by_amount(dec!("1"), stx, |builder, bucket_id| {
+            builder.call_method(
+                component,
+                "stake",
+                args!(
+                    scrypto::resource::Bucket(bucket_id)
+                ),
+            )
+        })
+        .call_method(
+            account_component,
+            "deposit_batch",
+            args!(Expression::entire_worktop()),
+        )
+        .build();
+    let receipt : TransactionReceipt = test_runner.execute_manifest_ignoring_fee(manifest, vec![public_key.into()]);
+    println!("{:?}\n", receipt);
+
+ 
+    /*
+
+    let outcome = &receipt
+    .expect_commit()
+    .outcome;
+
+    match outcome {
+        TransactionOutcome::Succes(output) => {
+            println!("output{:?}",output)
+        }
+        _ => {  println!("output error") }
+        
+    }
+    */
+        
+    receipt.expect_commit_success();
+
+
+
+
+    
 
     
 }
